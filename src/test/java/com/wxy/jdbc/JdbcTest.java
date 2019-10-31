cd0ec72ae0cb60c5571c733501f93bc98f1931db
wangxiaoyuan <243548880@qq.com>
2019-10-31 10:48:11 +0800

    jdbc

--- src/test/java/com/wxy/jdbc/JdbcTest.java
+++ src/test/java/com/wxy/jdbc/JdbcTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Author wangxiaoyuan
  * @Date 19-10-30 下午6:53
@@ -19,7 +22,23 @@ public class JdbcTest {
     private JdbcTemplate jdbcTemplate;
 
     @Test
+    public void createTable() {
+        String sql = "create table t_user(name varchar(20),age int(2))";
+        jdbcTemplate.execute(sql);
+        log.info("创建table...");
+    }
+
+    @Test
     public void insert() {
-        log.info("插入数据...");
+        String sql = "insert into t_user(name,age) values('张三',24)";
+        jdbcTemplate.execute(sql);
+        log.info("插入insert...");
+    }
+
+    @Test
+    public void queryList(){
+        String sql = "select * from t_user";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+        log.info("查询列表：list = {}",list);
     }
 }
